c3936e19f4887166be339e75c73ca294c54234bd
ycasayco-cloud <ycasayco@continental.edu.pe>
2026-08-27 12:12:58 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+
+app.MapGet("/", () =>
+{
+    return "API Sistema de Gestión funcionando";
+});
+
+
+app.MapGet("/api/cocteles", () =>
+{
+    return Results.Ok(new[]
+    {
+        new
+        {
+            dni = "72845631",
+            nombre = "Juan",
+            apellido = "Perez",
+            curso = "Diseno Web",
+            nota = 16
+        },
+        new
+        {
+            dni = "72845632",
+            nombre = "Maria",
+            apellido = "Lopez",
+            curso = "Diseno Web",
+            nota = 18
+        },
+        new
+        {
+            dni = "72845633",
+            nombre = "Pedro",
+            apellido = "Galbez",
+            curso = "App",
+            nota = 16
+        },
+        new
+        {
+            dni = "72845634",
+            nombre = "Maria",
+            apellido = "Mercedes",
+            curso = "Intro",
+            nota = 18
+        }
+    });
+});
+
+
+app.Run();
